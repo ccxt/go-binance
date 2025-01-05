9cb9c3178ef7fe572d155af74e16ac7f95b5ffb3
Ran <edding.suree@gmail.com>
2025-01-05 10:35:06 +0800

    combined channel for depthUpdate and trade

--- v2/websocket_service_custom.go
+++ v2/websocket_service_custom.go
@@ -1,18 +1,17 @@
 package binance
 
 import (
+	"encoding/json"
 	"fmt"
-	"log"
 	"strings"
 )
 
-
-func WsCombinedDepthAndTradeServe(symbol string, handler WsPartialDepthHandler, errHandler ErrHandler) (doneC, stopC chan struct{}, err error) {
-	endpoint := fmt.Sprintf("%s/%s@depth@trade", getWsEndpoint(), strings.ToLower(symbol))
-	return wsCombinedDepthAndTradeServe(endpoint, symbol, handler, errHandler)
+func WsCombinedDepthAndTradeServe(symbol string, depthHandler WsDepthHandler, tradeHandler WsTradeHandler, errHandler ErrHandler) (doneC, stopC chan struct{}, err error) {
+	endpoint := fmt.Sprintf("%s/%s@depth/%s@trade", getWsEndpoint(), strings.ToLower(symbol), strings.ToLower(symbol))
+	return wsCombinedDepthAndTradeServe(endpoint, depthHandler, tradeHandler, errHandler)
 }
 
-func wsCombinedDepthAndTradeServe(endpoint string, symbol string, handler WsPartialDepthHandler, errHandler ErrHandler) (doneC, stopC chan struct{}, err error) {
+func wsCombinedDepthAndTradeServe(endpoint string, depthHandler WsDepthHandler, tradeHandler WsTradeHandler, errHandler ErrHandler) (doneC, stopC chan struct{}, err error) {
 	cfg := newWsConfig(endpoint)
 	wsHandler := func(message []byte) {
 		j, err := newJSON(message)
@@ -20,29 +19,45 @@ func wsCombinedDepthAndTradeServe(endpoint string, symbol string, handler WsPart
 			errHandler(err)
 			return
 		}
-		log.Println(j)
-		event := new(WsPartialDepthEvent)
-		event.Symbol = symbol
-		event.LastUpdateID = j.Get("lastUpdateId").MustInt64()
-		bidsLen := len(j.Get("bids").MustArray())
-		event.Bids = make([]Bid, bidsLen)
-		for i := 0; i < bidsLen; i++ {
-			item := j.Get("bids").GetIndex(i)
-			event.Bids[i] = Bid{
-				Price:    item.GetIndex(0).MustString(),
-				Quantity: item.GetIndex(1).MustString(),
+		eventType := j.Get("e").MustString()
+		if eventType == "depthUpdate" {
+			event := new(WsDepthEvent)
+			stream := j.Get("stream").MustString()
+			symbol := strings.Split(stream, "@")[0]
+			event.Symbol = strings.ToUpper(symbol)
+			data := j.Get("data").MustMap()
+			event.Time, _ = data["E"].(json.Number).Int64()
+			event.LastUpdateID, _ = data["u"].(json.Number).Int64()
+			event.FirstUpdateID, _ = data["U"].(json.Number).Int64()
+			bidsLen := len(data["b"].([]interface{}))
+			event.Bids = make([]Bid, bidsLen)
+			for i := 0; i < bidsLen; i++ {
+				item := data["b"].([]interface{})[i].([]interface{})
+				event.Bids[i] = Bid{
+					Price:    item[0].(string),
+					Quantity: item[1].(string),
+				}
 			}
-		}
-		asksLen := len(j.Get("asks").MustArray())
-		event.Asks = make([]Ask, asksLen)
-		for i := 0; i < asksLen; i++ {
-			item := j.Get("asks").GetIndex(i)
-			event.Asks[i] = Ask{
-				Price:    item.GetIndex(0).MustString(),
-				Quantity: item.GetIndex(1).MustString(),
+			asksLen := len(data["a"].([]interface{}))
+			event.Asks = make([]Ask, asksLen)
+			for i := 0; i < asksLen; i++ {
+	
+				item := data["a"].([]interface{})[i].([]interface{})
+				event.Asks[i] = Ask{
+					Price:    item[0].(string),
+					Quantity: item[1].(string),
+				}
 			}
+			depthHandler(event)
+		} else if eventType == "trade" {
+			event := new(WsTradeEvent)
+			err := json.Unmarshal(message, event)
+			if err != nil {
+				errHandler(err)
+				return
+			}
+			tradeHandler(event)
 		}
-		handler(event)
 	}
 	return wsServe(cfg, wsHandler, errHandler)
 }
